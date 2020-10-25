bd79eafeabb314826c8f9784247a97141d9a0934
lrm4475 <lrm4475@rit.edu>
2020-10-25 15:31:51 -0400

    Domomaker C fin

--- server/app.js
+++ server/app.js
@@ -9,8 +9,13 @@ const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 // B
 const session = require('express-session');
+// C
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const redis = require('redis');
+const csrf = require('csurf');
 
-const port = process.env.PORT || process.env.NODE_PORT || 3000;
+const port = process.env.PORT || process.env.NODE_PORT || 3001;
 
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
@@ -27,6 +32,23 @@ mongoose.connect(dbURL, mongooseOptions, (err) => {
   }
 });
 
+let redisURL = {
+  hostname: 'redis-19550.c228.us-central1-1.gce.cloud.redislabs.com',
+  port: '19550',
+};
+
+let redisPASS = '7PF1UA2mvSh3dSrCxJD5gJ7G8Tyf170K';
+if (process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  [, redisPASS] = redisURL.auth.split(':');
+}
+
+const redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPASS,
+});
+
 // pull in our routes
 const router = require('./router.js');
 
@@ -39,14 +61,30 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(session({
   key: 'sessionid',
+  store: new RedisStore({
+    client: redisClient,
+  }),
   secret: 'Domo Arigato',
   resave: true,
   saveUnintialized: true,
+  cookie: {
+    httpOnly: true,
+  }
 }));
+
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
+app.disable('x-powered-by');
 app.use(cookieParser());
+
+//C
+app.use(csrf());
+app.use((err, req, res, next) => {
+    if(err.code !== 'EBADCSRFTOKEN') return next(err);
+    console.log('Missing CSRF token');
+    return false;
+});
 
 router(app);
 
--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -4,12 +4,12 @@ const { Domo } = models;
 
 const makerPage = (req, res) => {
   Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
-      if(err){
-          console.log(err);
-          return res.status(400).json({error: 'An error occured.'});
-      }
-      
-      return res.render('app', {domos:docs});
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occured.' });
+    }
+
+    return res.render('app', { csrfToken: req.csrfToken(), domos: docs });
   });
 };
 
